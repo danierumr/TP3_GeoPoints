c237b8e8a173dfe66560886c387a66bba50dbd0c
Daniel <51673798+danierumr@users.noreply.github.com>
2025-05-09 09:32:26 -0300

    Fisrt commit

---
+++ app/src/main/java/inf311/daniel/tp3_geopoints/MainActivity.java
@@ -0,0 +1,52 @@
+package inf311.daniel.tp3_geopoints;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends ListActivity {
+
+    private final LatLng CASA = new LatLng(-20.75553442922549, -42.87802558671229);
+    private final LatLng DPI = new LatLng(-20.76450768533006, -42.8680712796368);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+
+        String menu [] = new String [] {"Casa", "Departamento", "Fechar"};
+        ArrayAdapter<String> arrAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu);
+        setListAdapter(arrAdapter);
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+
+        Intent it = new Intent(getBaseContext(), MapActivity.class);
+
+        switch (position){
+            case 0:
+
+                startActivity(it);
+                break;
+            case 1:
+
+                startActivity(it);
+                break;
+
+            default:
+                finish();
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/inf311/daniel/tp3_geopoints/MapActivity.java
@@ -0,0 +1,14 @@
+package inf311.daniel.tp3_geopoints;
+
+
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MapActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+    }
+}
